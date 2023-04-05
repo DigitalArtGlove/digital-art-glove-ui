b91f291b64091c30dc01b8bc2d8354d939e5639b
Beatrice Tam <72827596+btam6293@users.noreply.github.com>
2023-04-05 14:33:00 -0700

    move websockets to canvas file and pass position data as params

--- digital-art-glove-ui/pages/canvas.js
+++ digital-art-glove-ui/pages/canvas.js
@@ -1,8 +1,64 @@
+import React, { useEffect, useState } from 'react';
+
 import Head from 'next/head'
 import styles from '../styles/Canvas.module.css'
 import Sketch from './sketch'
 
+const URL_WEB_SOCKET1 = 'ws://localhost:8765/';
+const URL_WEB_SOCKET2 = 'ws://localhost:8766/';
+
 export default function Canvas() {
+
+  const [ws1, setWs1] = React.useState(null);
+  const [ws2, setWs2] = React.useState(null);
+
+  const [cvData, setCVData] = React.useState([]);
+  const [serialData, setSerialData] = React.useState([]);
+
+  useEffect(() => {
+    const wsClient1 = new WebSocket(URL_WEB_SOCKET1);
+    wsClient1.onopen = () => {
+      setWs1(wsClient1);
+      console.log("ws 1 open\n");
+    };
+    wsClient1.onclose = () => console.log("ws 1 closed\n");
+    return () => {
+      wsClient1.close();
+    };
+  }, []);
+
+  useEffect(() => {
+    const wsClient2 = new WebSocket(URL_WEB_SOCKET2);
+    wsClient2.onopen = () => {
+      setWs2(wsClient2);
+      console.log("ws 2 open\n");
+    };
+    wsClient2.onclose = () => console.log("ws 2 closed\n");
+    return () => {
+      wsClient2.close();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (ws1) {
+      ws1.onmessage = (evt) => {
+        // console.log(evt);
+        const d = evt.data.split(" ");
+        setCVData(d);
+      };
+    }
+  }, [ws1]);
+
+  useEffect(() => {
+    if (ws2) {
+      ws2.onmessage = (evt) => {
+        // console.log(evt);
+        const d = evt.data.split(" ");
+        setSerialData(d);
+      };
+    }
+  }, [ws2]);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,9 +67,11 @@ export default function Canvas() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <div className={styles.canvas}> 
         <div className={styles.main}>
-          <Sketch />
+          <Sketch cv_data={cvData} serial_data={serialData}/>
         </div>
+      </div>
 
       <footer className={styles.footer}>
         Created by Beatrice Tam, Kaylee Jung, Mika Nogami, and Josiann Zhou
--- digital-art-glove-ui/pages/sketch.js
+++ digital-art-glove-ui/pages/sketch.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
-import styles from '../styles/Home.module.css'
-import Cursor from './cursor'
+import styles from '../styles/Home.module.css';
 import ColourBox from './colourBox';
 
 const URL_WEB_SOCKET1 = 'ws://localhost:8765/';
@@ -13,10 +12,8 @@ const Sketch = dynamic( () => import('react-p5'), { ssr: false } );
 const yaw = 0;
 const pitch = 1;
 const roll = 2;
-// const index_flex = 3;
 const middle_flex = 3;
 const ring_flex = 4;
-// const index_force = 6;
 const middle_force = 5;
 const ring_force = 6;
 const pinky_force = 7;
@@ -28,9 +25,9 @@ const y_coord = 1;
 // Same as coordRes used in CV file that determines the resolution (number of digits) we send to front-end
 const coordRes = Math.pow(2,10);
 
-const MySketch = () => {
-  const [ws1, setWs1] = React.useState(null);
-  const [ws2, setWs2] = React.useState(null);
+const MySketch = (data) => {
+  // const [ws1, setWs1] = React.useState(null);
+  // const [ws2, setWs2] = React.useState(null);
 
   const [update, setUpdate] = React.useState(false);
   const [clear, setClear] = React.useState(false);
@@ -57,7 +54,7 @@ const MySketch = () => {
     const height = window.innerHeight;
     const width = window.innerWidth;
     let c = p5.createCanvas(width, height).parent(canvasParentRef);
-    // p5.background('white');
+    p5.background('white');
     p5.stroke(0);
     p5.strokeWeight(5);
     var button = p5.createButton('Clear');
@@ -69,70 +66,79 @@ const MySketch = () => {
   };
 
   useEffect(() => {
-    const wsClient1 = new WebSocket(URL_WEB_SOCKET1);
-    wsClient1.onopen = () => {
-      setWs1(wsClient1);
-      console.log("ws 1 open\n");
-    };
-    wsClient1.onclose = () => console.log("ws 1 closed\n");
-    return () => {
-      wsClient1.close();
-    };
-  }, []);
-
-  useEffect(() => {
-    const wsClient2 = new WebSocket(URL_WEB_SOCKET2);
-    wsClient2.onopen = () => {
-      setWs2(wsClient2);
-      console.log("ws 2 open\n");
-    };
-    wsClient2.onclose = () => console.log("ws 2 closed\n");
-    return () => {
-      wsClient2.close();
-    };
-  }, []);
-
-  useEffect(() => {
-    if (ws1) {
-      ws1.onmessage = (evt) => {
-        // console.log(evt);
-        const d = evt.data.split(" ");
-        setUpdate(true);
-        setPos([Number(d[x_coord]/coordRes*window.innerWidth), Number(d[y_coord]/coordRes*window.innerHeight)]);
-        setxPos(Number(d[0]/coordRes*window.innerWidth));
-        setyPos(Number(d[1]/coordRes*window.innerHeight));
-      };
+    if(data) {
+      setUpdate(true);
+      setPos([Number(data.cv_data[x_coord]/coordRes*window.innerWidth), Number(data.cv_data[y_coord]/coordRes*window.innerHeight)]);
+      setxPos(Number(data.cv_data[x_coord]/coordRes*window.innerWidth));
+      setyPos(Number(data.cv_data[y_coord]/coordRes*window.innerHeight));
     }
-  }, [ws1]);
+  }, [data]);
 
-  useEffect(() => {
-    if (ws2) {
-      ws2.onmessage = (evt) => {
-        // console.log(evt);
-        const d = evt.data.split(" ");
+  // useEffect(() => {
+  //   const wsClient1 = new WebSocket(URL_WEB_SOCKET1);
+  //   wsClient1.onopen = () => {
+  //     setWs1(wsClient1);
+  //     console.log("ws 1 open\n");
+  //   };
+  //   wsClient1.onclose = () => console.log("ws 1 closed\n");
+  //   return () => {
+  //     wsClient1.close();
+  //   };
+  // }, []);
 
-        if (d[middle_force] > 125) {
-          setSelect(true);
-        } else {
-          setSelect(false);
-        }
+  // useEffect(() => {
+  //   const wsClient2 = new WebSocket(URL_WEB_SOCKET2);
+  //   wsClient2.onopen = () => {
+  //     setWs2(wsClient2);
+  //     console.log("ws 2 open\n");
+  //   };
+  //   wsClient2.onclose = () => console.log("ws 2 closed\n");
+  //   return () => {
+  //     wsClient2.close();
+  //   };
+  // }, []);
 
-        if (d[ring_force] > 125) {
-          setErase(true);
-        } else {
-          setErase(false);
-        }
+  // useEffect(() => {
+  //   if (ws1) {
+  //     ws1.onmessage = (evt) => {
+  //       // console.log(evt);
+  //       const d = evt.data.split(" ");
+  //       setUpdate(true);
+  //       setPos([Number(d[x_coord]/coordRes*window.innerWidth), Number(d[y_coord]/coordRes*window.innerHeight)]);
+  //       setxPos(Number(d[0]/coordRes*window.innerWidth));
+  //       setyPos(Number(d[1]/coordRes*window.innerHeight));
+  //     };
+  //   }
+  // }, [ws1]);
 
-        if (d[pinky_force] > 200 && prevPinky < 200) {
-          setChangeColour(true);
-        } else {
-          setChangeColour(false);
-          setCurrentChangeColourState(false);
-        }
+  // useEffect(() => {
+  //   if (ws2) {
+  //     ws2.onmessage = (evt) => {
+  //       // console.log(evt);
+  //       const d = evt.data.split(" ");
 
-      };
-    }
-  }, [ws2]);
+  //       if (d[middle_force] > 125) {
+  //         setSelect(true);
+  //       } else {
+  //         setSelect(false);
+  //       }
+
+  //       if (d[ring_force] > 125) {
+  //         setErase(true);
+  //       } else {
+  //         setErase(false);
+  //       }
+
+  //       if (d[pinky_force] > 200 && prevPinky < 200) {
+  //         setChangeColour(true);
+  //       } else {
+  //         setChangeColour(false);
+  //         setCurrentChangeColourState(false);
+  //       }
+
+  //     };
+  //   }
+  // }, [ws2]);
 
   const draw = (p5) => {
     // background_colour 
